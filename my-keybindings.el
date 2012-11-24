8a517ebc400383472cbacfbbefdbb0fa1ddd7a27
Chris Thompson <chris.thompson@walkerart.org>
2012-11-24 15:25:12 -0600

    keys need cleanup

--- my-keybindings.el
+++ my-keybindings.el
@@ -27,7 +27,7 @@
 
 ;;--edit in app --(move to app key maps someday)
 (ergoemacs-global-set-key (kbd "C-x 9") 'dired-omit-mode)
-(ergoemacs-global-set-key (kbd "C-x <f9>") 'find-lisp-find-dired-subdirectories)
+
 (ergoemacs-global-set-key (kbd "<C-tab>") 'magit-show-only-files)
 (ergoemacs-global-set-key (kbd "C-c li") 'org-clock-in)
 (ergoemacs-global-set-key (kbd "C-c lo") 'org-clock-out)
@@ -45,7 +45,7 @@
 (ergoemacs-global-set-key "\M-o" 'ido-switch-buffer)
 (ergoemacs-global-set-key "\M-v" 'ibuffer)
 (ergoemacs-global-set-key (kbd "C-c C-r") 'rename-file-and-buffer)
-(ergoemacs-global-set-key (kbd "M-'") 'switch-window)
+(ergoemacs-global-set-key (kbd "M-'") 'switch-to-buffer-other-window)
 (ergoemacs-global-set-key (kbd "C-c e b") 'eval-buffer)
 (ergoemacs-global-set-key (kbd "C-x k") 'close-current-buffer)
 ;; apps
@@ -54,6 +54,7 @@
 (ergoemacs-global-set-key (kbd "C-c a") 'org-agenda)
 (ergoemacs-global-set-key (kbd "<f11>") 'fullscreen-toggle)
 (ergoemacs-global-set-key (kbd "<f6>") 'whitespace-mode)
+(ergoemacs-global-set-key (kbd "M-<f6>") 'whitespace-cleanup)
 (ergoemacs-global-set-key (kbd "<f5>") 'flyspell-mode)
 (ergoemacs-global-set-key (kbd "M-<f7>") 'pianobar)
 (ergoemacs-global-set-key (kbd "<f9>") 'flymake-mode)
@@ -70,7 +71,7 @@
 (define-key global-map (kbd "C-s") 'save-buffer)
 
 
-(eval-after-load 'slime 
+(eval-after-load 'slime
   '(progn
      (define-key slime-mode-map (kbd "M-n") 'forward-char)
      ))
@@ -86,9 +87,7 @@
                            (define-key dired-mode-map "e" `dired-up-directory)
                            (define-key dired-mode-map "o" `dired-display-file)
                            (define-key dired-mode-map "k" `dired-kill-subdir)))
-
-(eval-after-load 'org-remember-mode '(progn
-                 (local-set-key (kbd "C-2") ctl-x-map)))
+                           ;; v = view-mode
 
 (eval-after-load 'rspec
   '(progn
@@ -97,22 +96,32 @@
 
 ;;try: find-lisp-find-dired
 ;;try: find-lisp-find-dired-subdirectories
-(ergoemacs-global-set-key (kbd "C-8") 'find-file-at-point)
+
 (ergoemacs-global-set-key (kbd "C-\-") 'toggle-hiding)
 (ergoemacs-global-set-key (kbd "C-\\") 'toggle-selective-display)
 ;; (eval-after-load 'w3m
 ;;   '(progn
-;;      (define-key w3m-mode-map "f" 'w3m-next-form)    
+;;      (define-key w3m-mode-map "f" 'w3m-next-form)
 ;;     ))
 
 
 ;; (org-set-tags-to)
-(ergoemacs-global-set-key (kbd "M-1") 'ioccur)
+
 (eval-after-load 'ioccur '(progn
   (define-key ioccur-mode-map "d" `ioccur-jump-without-quit)
   (define-key ioccur-mode-map "k" `ioccur-jump-and-quit)
   (define-key ioccur-mode-map "n" `ioccur-next-line)
-  (define-key ioccur-mode-map "p" `ioccur-precedent-line)))
+  (define-key ioccur-mode-map "p" `ioccur-precedent-line)
+  (define-key ioccur-mode-map "N" `ioccur-scroll-down)
+  (define-key ioccur-mode-map "P" `ioccur-scroll-up)))
+
+;; jump points
+(ergoemacs-global-set-key (kbd "C-1") 'ioccur)
+(ergoemacs-global-set-key (kbd "C-2") 'find-tag)
+(ergoemacs-global-set-key (kbd "C-3") 'find-file-at-point)
+(ergoemacs-global-set-key (kbd "C-4") 'find-grep)
+(ergoemacs-global-set-key (kbd "C-5") 'find-lisp-find-dired)
+(ergoemacs-global-set-key (kbd "C-x <f9>") 'find-lisp-find-dired-subdirectories)
 
 ;; (flyspell-correct-word-before-point)
 ;;(ergoemacs-global-set-key "\" 'bookmark-jump)
@@ -125,7 +134,7 @@
 
 (eval-after-load 'ibuffer
   '(progn
-     (define-key ibuffer-mode-map "e" 'ibuffer-ediff-marked-buffers) 
+     (define-key ibuffer-mode-map "e" 'ibuffer-ediff-marked-buffers)
      (define-key ibuffer-mode-map (kbd "M-RET") 'ibuffer-bs-toggle-all)
      (define-key ibuffer-mode-map (kbd "TAB") 'ibuffer-toggle-filter-group)
      ))
