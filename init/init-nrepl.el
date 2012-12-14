a992a1e6e40362eb5b2332556292c28868babf04
Chris Thompson <chris.thompson@walkerart.org>
2012-12-14 11:32:38 -0600

    nrepl wip

--- init/init-nrepl.el
+++ init/init-nrepl.el
@@ -1,6 +1,6 @@
 (define-key nrepl-mode-map (kbd "C-.") 'nrepl-previous-input)
 
 (define-key nrepl-mode-map (kbd "M-.") 'backward-kill-word)
-(undefine-key nrepl-mode-map (kbd "M-.")
+;;(undefine-key nrepl-mode-map (kbd "M-."))
 (define-key nrepl-mode-map (kbd "C-j") 'nrepl-jump)
 
