2c3122edcbec953c5fe4afaea6e31fa97b58158d
teaforthecat <teaforthecat@gmail.com>
2012-12-14 14:12:57 -0600

    nrepl init

--- init/init-nrepl.el
+++ init/init-nrepl.el
@@ -1,6 +1,15 @@
 (define-key nrepl-mode-map (kbd "C-.") 'nrepl-previous-input)
 
 (define-key nrepl-mode-map (kbd "M-.") 'backward-kill-word)
-(undefine-key nrepl-mode-map (kbd "M-.")
+;;(undefine-key nrepl-mode-map (kbd "M-."))
 (define-key nrepl-mode-map (kbd "C-j") 'nrepl-jump)
 
+
+(add-hook 'nrepl-interaction-mode-hook
+  'nrepl-turn-on-eldoc-mode)
+
+(setq nrepl-popup-stacktraces nil)
+
+(add-to-list 'same-window-buffer-names "*nrepl*")
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
