40d2010912d45ead5dfa4caab4ab6c30d46d3c35
teaforthecat <teaforthecat@gmail.com>
2013-01-23 22:00:12 -0600

    add init-ibuffer

--- init/init-ibuffer.el
+++ init/init-ibuffer.el
@@ -22,3 +22,14 @@
 			 (name . ".el$")
 			 (name . "^\\*scratch\\*$")
 			 (name . "^\\*Messages\\*$")))))))
+
+
+;; from: http://curiousprogrammer.wordpress.com/2009/04/02/ibuffer/
+(defun ibuffer-ediff-marked-buffers ()
+  (interactive)
+  (let* ((marked-buffers (ibuffer-get-marked-buffers))
+         (len (length marked-buffers)))
+    (unless (= 2 len)
+      (error (format "%s buffer%s been marked (needs to be 2)"
+                     len (if (= len 1) " has" "s have"))))
+    (ediff-buffers (car marked-buffers) (cadr marked-buffers))))
