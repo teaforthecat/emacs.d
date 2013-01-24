0c4a5395ef6c62689f5739b6d6c3ea3dd9b0af22
teaforthecat <teaforthecat@gmail.com>
2013-01-23 22:00:43 -0600

    reorganize my-settings

--- my-settings.el
+++ my-settings.el
@@ -11,12 +11,6 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
-
-(setq diary-show-holidays-flag nil) ;calendar
-(setq diary-file "~/org/.diary") ;calendar
-(add-hook 'diary-hook 'appt-make-list) ;diary-lib ;appt
-(appt-activate 1) ;appt
-
 (when (eq system-type 'darwin) ;; homebrew
   (setq mac-option-modifier 'alt)
   (setq mac-command-modifier 'meta)
@@ -28,16 +22,26 @@
 
 (setq require-final-newline  t)  
 
+
+
 (add-hook 'write-contents-functions 'delete-trailing-whitespace)
+
+(add-hook 'after-init-hook 'color-theme-subtle-hacker)
 
 (add-hook 'after-init-hook '(lambda () (org-agenda-list)
                               (switch-to-buffer-other-window 
                                (or (get-buffer "timelog.org")
                                    (get-buffer "*scratch*") ))))
 
-(add-hook 'after-init-hook 'color-theme-subtle-hacker)
+
+;; Diary
+(setq diary-show-holidays-flag nil) ;calendar
+(setq diary-file "~/org/.diary") ;calendar
+(add-hook 'diary-hook 'appt-make-list) ;diary-lib ;appt
+(appt-activate 1) ;appt
 
 
+;; Shell
 ;; not sure why I have this
 (add-hook 'comint-mode-hook 
           (lambda () 
@@ -45,21 +49,24 @@
                  (or (file-remote-p default-directory) ""))))
 
 (add-hook 'shell-mode-hook 'dirtrack-mode)
+;; ;; not sure I want this
+;; (add-hook 'shell-mode-hook 
+;;           (lambda () 
+;;             (setq comint-prompt-regexp "^[^#$%>\n]*[#$%>] *")
+;;             (setq scroll-conservatively 10)))
 
-;; really neccessary?
-;; (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; Dired
+;; (setq dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$")
+(setq dired-omit-files "^\\..*|.*~$")
 
 (add-hook 'dired-mode-hook
           (lambda ()
             (dired-omit-mode 1)))
-
-;; not sure I want this
-(add-hook 'shell-mode-hook 
-          (lambda () 
-            (setq comint-prompt-regexp "^[^#$%>\n]*[#$%>] *")
-            (setq scroll-conservatively 10)))
-
 (put 'dired-find-alternate-file 'disabled nil)
+
+
+
 
 (server-mode t)
 
