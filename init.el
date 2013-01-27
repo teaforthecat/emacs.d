5c59a862a577af666b307fe94b11789dd77f7c40
teaforthecat <teaforthecat@gmail.com>
2013-01-27 16:38:35 -0600

    initialize emacs within init and init-* files

--- init.el
+++ init.el
@@ -36,11 +36,22 @@
 			    (progn
 			      (el-get-emacswiki-refresh el-get-recipe-path-emacswiki)
 			      ))))
+   (:name ediff
+          :after (progn ()
+                        (setq ediff-diff-options "-w")
+                        (setq ediff-split-window-function 'split-window-horizontally)
+                        (setq ediff-window-setup-function 'ediff-setup-windows-plain)))
    (:name ibuffer
           :type builtin)
    (:name ido
           :type builtin)
    (:name tramp
+          :type builtin)
+   (:name dired+
+          :type builtin)
+   (:name shell
+          :type builtin)
+   (:name diary-lib
           :type builtin)
    (:name yasnippet
           :before (progn
@@ -51,9 +62,6 @@
    (:name autopair
           :after (progn ()
                        (autopair-global-mode 1)))
-   (:name haml-mode
-          :after (progn ()
-                       (setq haml-indent-offset 2)))
    (:name emacs-w3m
           :after (progn ()
                         (setq browse-url-browser-function 'w3m-browse-url)))
@@ -85,6 +93,11 @@
           :after (progn
                    (add-to-list 'auto-mode-alist '("\\.coffee" . coffee-mode))
                    (add-to-list 'auto-mode-alist '("\\.coffee\\.erb" . coffee-mode))))
+   (:name  undo-tree
+          :after (progn
+                   (setq undo-tree-mode-lighter "")
+                   (global-undo-tree-mode)))
+
    (:name uniquify
           :type builtin
           :features (uniquify)
@@ -98,7 +111,7 @@
       '(anything
 		 browse-kill-ring
          cl-lib clojure-mode color-theme color-theme-ubuntu2
-		 dired+ django-mode
+		 dash dired+ django-mode
 		 emacs-w3m
 		 fullscreen feature-mode
 		 haml-mode htmlize
@@ -112,6 +125,7 @@
 		 redo+ rinari rhtml-mode rspec-mode ruby-end
          ;ruby-electric conficts with pair
          rvm
+         smooth-scrolling
 		 wanderlust
 		 yaml-mode))
 
@@ -133,20 +147,93 @@
 
 
 (require 'my-keybindings)
-(require 'my-settings)
 
+;; from magnars
+;; Allow pasting selection outside of Emacs
+(setq x-select-enable-clipboard t)
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
+;; Real emacs knights don't use shift to mark things
+(setq shift-select-mode nil)
+;; Transparently open compressed files
+(auto-compression-mode t)
 
+;; Answering just 'y' or 'n' will do
+(defalias 'yes-or-no-p 'y-or-n-p)
+;; UTF-8 please
+(setq locale-coding-system 'utf-8) ; pretty
+(set-terminal-coding-system 'utf-8) ; pretty
+(set-keyboard-coding-system 'utf-8) ; pretty
+(set-selection-coding-system 'utf-8) ; please
+(prefer-coding-system 'utf-8) ; with sugar on top
 
+;; Remove text in active region if inserting text
+(delete-selection-mode 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(whitespace-space ((t (:foreground "darkgray")))))
+;; Always display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+(show-paren-mode t)
+
+;; Lines should be 80 characters wide, not 72
+(setq fill-column 80)
+
+;; Save a list of recent files visited. (open recent file with C-x f)
+(recentf-mode 1)
+(setq recentf-max-saved-items 100) ;; just 20 is too recent
+
+;; Undo/redo window configuration with C-c <left>/<right>
+(winner-mode 1)
+
+;; Never insert tabs
+(set-default 'indent-tabs-mode nil)
+
+;; Show me empty lines after buffer end
+(set-default 'indicate-empty-lines t)
+
+;; Easily navigate sillycased words
+(global-subword-mode 1)
+
+;; Don't break lines for me, please
+(setq-default truncate-lines t)
+
+;; Sentences do not need double spaces to end. Period.
+(set-default 'sentence-end-double-space nil)
+
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+;; UTF-8 please
+(setq locale-coding-system 'utf-8) ; pretty
+(set-terminal-coding-system 'utf-8) ; pretty
+(set-keyboard-coding-system 'utf-8) ; pretty
+(set-selection-coding-system 'utf-8) ; please
+(prefer-coding-system 'utf-8) ; with sugar on top
+
+(when (eq system-type 'darwin) ;; homebrew
+  (setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'meta)
+  
+  )
+
+(setq temporary-file-directory "~/.emacs.d/tmp/")
+
+(add-hook 'after-init-hook '(lambda () (org-agenda-list)
+                              (switch-to-buffer-other-window 
+                               (or (get-buffer "timelog.org")
+                                   (get-buffer "*scratch*") ))))
+
+(add-hook 'write-contents-functions 'delete-trailing-whitespace)
+(setq require-final-newline  t)  
+
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(server-mode t)
