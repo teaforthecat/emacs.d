9dc303898cce69daf45487caa8c057a6baa7a63e
teaforthecat <teaforthecat@gmail.com>
2013-01-27 19:53:17 -0600

    confirm no file

--- minimals.el
+++ minimals.el
@@ -1,7 +1,6 @@
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
 
-(setq confirm-nonexistent-file-or-buffer nil)
 (setq compilation-ask-about-save nil)
 (setq compilation-save-buffers-predicate '(lambda () nil))
 (fset 'yes-or-no-p 'y-or-n-p)
