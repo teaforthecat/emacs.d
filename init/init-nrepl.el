6d89dc44168ac35c44c120b5219709d4afac8bb8
teaforthecat <teaforthecat@gmail.com>
2013-02-22 09:23:58 -0600

    more nrepl settings

--- init/init-nrepl.el
+++ init/init-nrepl.el
@@ -4,3 +4,12 @@
 ;;(undefine-key nrepl-mode-map (kbd "M-."))
 (define-key nrepl-mode-map (kbd "C-j") 'nrepl-jump)
 
+
+(add-hook 'nrepl-interaction-mode-hook
+  'nrepl-turn-on-eldoc-mode)
+
+(setq nrepl-popup-stacktraces nil)
+
+(add-to-list 'same-window-buffer-names "*nrepl*")
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
