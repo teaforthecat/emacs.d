e882c0e8bfd568f4137995ccaa988651c151afbf
teaforthecat <teaforthecat@gmail.com>
2013-02-24 12:52:15 -0600

    apply none-ergo

--- my-keybindings.el
+++ my-keybindings.el
@@ -38,6 +38,144 @@
 (M z 'toggle-letter-case)               ;timid
 (M s 'ace-jump-mode)                    ;new
 (M - 'comment-dwim)                     ;timid
+(M x 'execute-extended-command)         ;timid
+(M f 'nil)                              ;empty
+
+;(M \ 'yas/expand)                       ;replace
+(M = 'count-words-region)
+(M / 'dabbrev-expand)                   ;default
+(M "]" 'nil)                            ;empty
+(M "[" 'nil)                            ;empty
+
+(M 0 'delete-window)
+(M 9 'nil)
+(M 8 'extend-selection)                 ;important
+(M 7 'nil)
+(M 7 'nil)
+(M 6 'nil)
+
+; left hand
+(M 5 'query-replace)
+(M 4 'split-window-vertically)
+(M 3 'delete-other-windows)
+(M 2 'nil)
+(M 1 'nil)
+(M "`" 'switch-to-next-frame)           ;timid
+(M "'" 'ido-switch-buffer-other-window) ;timid
+(M o   'ido-switch-buffer)              ;muscle conflicts with M-a
+(M a   'execute-extended-command)       ;dup of M-x
+
+; edit (left hand)
+(M "," 'shrink-whitespaces)             ;timid
+(M "." 'subword-backward-kill)
+(M p   'subword-kill)
+(M y   'call-keyword-completion)        ;timid
+(M i   'kill-line)                      ;timid
+(M u   'delete-char)
+(M e   'autopair-backspace)
+(M ";" 'undo-tree-undo)
+(M q   'kill-region)
+(M j   'clipboard-kill-ring-save)
+(M k   'clipboard-yank)
+
+(M <f1> 'nil)
+(M <f2> 'nil)
+(M <f3> 'nil)
+(M <f4> 'nil)
+(M <f4> 'nil)
+(M <f5> 'flyspell-correct-word-before-point)
+(M <f6> 'whitespace-cleanup)
+(M <f7> 'pianobar)
+(M <f8> 'nil)
+(M <f9> 'nil)
+(M <f10> 'nil)
+(M <f11> 'nil)
+(M <f12> 'nil)
+
+
+
+;--------------
+
+;control
+
+; right hand
+(C t 'move-cursor-next-pane)
+(C h 'move-cursor-previous-pane)
+(C n 'new-empty-buffer)                 ;remove
+(C s 'save-buffer)
+(C - 'toggle-hiding)                    ;void
+(C b 'nil)
+(C m 'autopair-newline)                  ;timid
+(C w 'close-current-buffer)
+(C v 'nil)
+(C z 'nil)
+(C = 'select-text-in-quote)             ;important
+(C / 'undo-tree-undo)                   ;dup of M-;
+(C l 'nil)
+(C r 'comment-or-uncomment-region)
+;(C c 'prefix)
+
+;; might be causing problems
+;(C g 'keyboard-quit)
+
+(C f 'sudo-find-file)
+(C "]" 'abort-recursive-edit)           ;timid
+;(C "[" 'escape)                        ;built-in
+(C 9 'nil)
+(C 8 'nil)
+(C 7 'nil)
+(C 6 'nil)
+
+; left hand
+(C 5 'find-lisp-find-dired)             ;dup of M-5
+(C 4 'find-grep)                        ;dup
+(C 3 'find-file-at-point)               ;dup
+(C 2 'find-tag)                         ;dup
+(C 1 'ioccur)                           ;dup
+
+(C 5 'find-lisp-find-dired)             ;important
+(C 4 'find-grep)                        ;important
+(C 3 'find-file-at-point)               ;important
+(C 2 'find-tag-name)                    ;important
+(C 1 'ioccur)                           ;important
+
+(C "'" 'nil)
+(C "," 'nil)
+(C "." 'comint-previous-input)
+(C p 'nil)
+(C y 'nil)
+(C i 'indent-for-tab-command)
+;(C u 'universal-argument)               ;built-in
+(C e 'comint-next-input)
+(C o 'ido-find-file)                    ;important
+(C a 'mark-whole-buffer)
+(C <tab> 'magit-show-only-files)
+
+(C ";" 'nil)
+;(C q 'quoted-insert)                   ;built-in
+(C j 'nil)
+(C k 'kill-whole-line)
+;(C x 'prefix)                          ;prefix
+;(C <f2> 'void)
+;(C <f3> 'mac)
+(C <f4> 'nil)
+(C <f5> 'nil)
+(C <f6> 'nil)
+(C <f7> 'nil)
+;(C <f8> 'mac)???
+(C <f7> 'nil)
+(C <f8> 'nil)
+(C <f9> 'nil)
+(C <f10> 'nil)
+(C <f11> 'nil)
+(C <f12> 'nil)
+
+
+
+; function keys
+(global-set-key (kbd "<f6>") 'whitespace-mode)
+(global-set-key (kbd "<f5>") 'flyspell-mode)
+(global-set-key (kbd "<f9>") 'flymake-mode)
 
 
 
@@ -55,60 +193,71 @@
 (ergoemacs-global-set-key (kbd "C-p") 'nil)
 
 ;; move
-(ergoemacs-global-set-key "\M-d" 'beginning-of-buffer)
-(ergoemacs-global-set-key "\M-D" 'end-of-buffer)
-(ergoemacs-global-set-key "\M-H" 'move-beginning-of-line)
-(ergoemacs-global-set-key "\M-N" 'move-end-of-line)
-(ergoemacs-global-set-key (kbd "C-x -") 'goto-line)
-(ergoemacs-global-set-key (kbd "M-O") 'isearch-occur)
-(ergoemacs-global-set-key (kbd "M-C-S") 'isearch-other-window)
+;; (ergoemacs-global-set-key "\M-d" 'beginning-of-buffer)
+
+;; TODO set these
+;; (ergoemacs-global-set-key "\M-D" 'end-of-buffer)
+;; (ergoemacs-global-set-key "\M-H" 'move-beginning-of-line)
+;; (ergoemacs-global-set-key "\M-N" 'move-end-of-line)
+;; (ergoemacs-global-set-key (kbd "C-x -") 'goto-line)
+;; (ergoemacs-global-set-key (kbd "M-O") 'isearch-occur)
+;; (ergoemacs-global-set-key (kbd "M-C-S") 'isearch-other-window)
+;; (ergoemacs-global-set-key "\M-\\" 'yas/expand)
+;; (ergoemacs-global-set-key "\C-k" 'kill-whole-line)
+;; (ergoemacs-global-set-key (kbd "C-M-,") 'kmacro-call-macro)
+;; (ergoemacs-global-set-key (kbd "C-x 9") 'dired-omit-mode)
+;; (ergoemacs-global-set-key (kbd "C-c li") 'org-clock-in)
+;; (ergoemacs-global-set-key (kbd "C-c lo") 'org-clock-out)
+;; (ergoemacs-global-set-key (kbd "C-c lu") 'org-clock-update-time-maybe)
+;; (ergoemacs-global-set-key (kbd "<M-kp-right>") 'org-table-insert-column)
+;; (ergoemacs-global-set-key (kbd "<M-kp-down>") 'org-table-insert-row)
+;; (ergoemacs-global-set-key (kbd "C-x rv") 'list-registers)
+;; (ergoemacs-global-set-key "\M-X" 'dired)
+;; (ergoemacs-global-set-key (kbd "C-c e b") 'eval-buffer)
+;; (ergoemacs-global-set-key (kbd "C-x k") 'close-current-buffer)
+;; (ergoemacs-global-set-key (kbd "C-c C-r") 'rename-file-and-buffer)
+;; (ergoemacs-global-set-key (kbd "C-c r") 'org-capture)
+;; (ergoemacs-global-set-key (kbd "C-x g") 'magit-status)
+;; (ergoemacs-global-set-key (kbd "C-c a") 'org-agenda)
+;; (global-set-key (kbd "C-c C-x C-j") 'org-clock-goto)
+
+
+
+
+;; (ergoemacs-global-set-key (kbd "<C-tab>") 'magit-show-only-files)
+
 ;; (ergoemacs-global-set-key (kbd "C-t") 'move-cursor-next-pane)
 ;; (ergoemacs-global-set-key (kbd "C-h") 'move-cursor-previous-pane)
 
 ;; (ergoemacs-global-set-key (kbd "C-f") 'sudo-find-file)
 
 ;; edit
-(ergoemacs-global-set-key "\M-\\" 'yas/expand)
-(ergoemacs-global-set-key "\M-j" 'clipboard-kill-ring-save)
-(ergoemacs-global-set-key "\M-k" 'clipboard-yank)
-(ergoemacs-global-set-key "\C-k" 'kill-whole-line)
-(ergoemacs-global-set-key (kbd "C-.") 'comint-previous-input)
-(ergoemacs-global-set-key "\C-e" 'comint-next-input)
-(ergoemacs-global-set-key (kbd "C-r") 'comment-or-uncomment-region)
-(ergoemacs-global-set-key (kbd "M-<f5>") 'flyspell-correct-word-before-point)
-(ergoemacs-global-set-key (kbd "C-=") 'select-text-in-quote)
-(ergoemacs-global-set-key (kbd "C-M-,") 'kmacro-call-macro)
+
+;; (ergoemacs-global-set-key "\M-j" 'clipboard-kill-ring-save)
+;; (ergoemacs-global-set-key "\M-k" 'clipboard-yank)
+
+;; (ergoemacs-global-set-key (kbd "C-.") 'comint-previous-input)
+;; (ergoemacs-global-set-key "\C-e" 'comint-next-input)
+;; (ergoemacs-global-set-key (kbd "C-r") 'comment-or-uncomment-region)
+;; (ergoemacs-global-set-key (kbd "M-<f5>") 'flyspell-correct-word-before-point)
+;; (ergoemacs-global-set-key (kbd "C-=") 'select-text-in-quote)
+
 ;;(ergoemacs-global-set-key (kbd "M-e") 'autopair-backward-delete) ;;except minibuffer-map
 ;;(ergoemacs-global-set-key (kbd "M-e") 'delete-backward-char-untabify) ?
 
 
 ;;--edit in app --(move to app key maps someday)
-(ergoemacs-global-set-key (kbd "C-x 9") 'dired-omit-mode)
-
-(ergoemacs-global-set-key (kbd "<C-tab>") 'magit-show-only-files)
-(ergoemacs-global-set-key (kbd "C-c li") 'org-clock-in)
-(ergoemacs-global-set-key (kbd "C-c lo") 'org-clock-out)
-(ergoemacs-global-set-key (kbd "C-c lu") 'org-clock-update-time-maybe)
-(ergoemacs-global-set-key (kbd "<M-kp-right>") 'org-table-insert-column)
-(ergoemacs-global-set-key (kbd "<M-kp-down>") 'org-table-insert-row)
-(ergoemacs-global-set-key (kbd "C-x rv") 'list-registers)
 
 
 ;;(ergoemacs-global-set-key (kbd "") 'org-insert-subheading)
 ;; buffer
-(ergoemacs-global-set-key "\M-X" 'dired)
-(ergoemacs-global-set-key "\M-w" 'next-buffer)
-(ergoemacs-global-set-key "\M-m" 'previous-buffer)
-(ergoemacs-global-set-key "\M-o" 'ido-switch-buffer)
-(ergoemacs-global-set-key "\M-v" 'ibuffer)
-(ergoemacs-global-set-key (kbd "C-c C-r") 'rename-file-and-buffer)
-(ergoemacs-global-set-key (kbd "M-'") 'switch-to-buffer-other-window)
-(ergoemacs-global-set-key (kbd "C-c e b") 'eval-buffer)
-(ergoemacs-global-set-key (kbd "C-x k") 'close-current-buffer)
+
+;; (ergoemacs-global-set-key "\M-w" 'next-buffer)
+;; (ergoemacs-global-set-key "\M-m" 'previous-buffer)
+;; (ergoemacs-global-set-key "\M-o" 'ido-switch-buffer)
+;; (ergoemacs-global-set-key "\M-v" 'ibuffer)
+;; (ergoemacs-global-set-key (kbd "M-'") 'switch-to-buffer-other-window)
 ;; apps
-(ergoemacs-global-set-key (kbd "C-c r") 'org-capture)
-(ergoemacs-global-set-key (kbd "C-x g") 'magit-status)
-(ergoemacs-global-set-key (kbd "C-c a") 'org-agenda)
 
 (if (string-match "apple-darwin" system-configuration)
     (global-set-key [f11] 'ns-toggle-fullscreen)
@@ -165,7 +314,7 @@
 ;;      (define-key w3m-mode-map "f" 'w3m-next-form)
 ;;     ))
 
-(global-set-key (kbd "C-c C-x C-j") 'org-clock-goto)
+
 ;; (org-set-tags-to)
 
 (eval-after-load 'ioccur '(progn
@@ -177,12 +326,12 @@
   (define-key ioccur-mode-map "P" `ioccur-scroll-up)))
 
 ;; jump points
-(ergoemacs-global-set-key (kbd "C-1") 'ioccur)
-(ergoemacs-global-set-key (kbd "C-2") 'find-tag)
-(ergoemacs-global-set-key (kbd "C-3") 'find-file-at-point)
-(ergoemacs-global-set-key (kbd "C-4") 'find-grep)
-(ergoemacs-global-set-key (kbd "C-5") 'find-lisp-find-dired)
-(ergoemacs-global-set-key (kbd "C-x <f9>") 'find-lisp-find-dired-subdirectories)
+;; (ergoemacs-global-set-key (kbd "C-1") 'ioccur)
+;; (ergoemacs-global-set-key (kbd "C-2") 'find-tag)
+;; (ergoemacs-global-set-key (kbd "C-3") 'find-file-at-point)
+;; (ergoemacs-global-set-key (kbd "C-4") 'find-grep)
+;; (ergoemacs-global-set-key (kbd "C-5") 'find-lisp-find-dired)
+;; (ergoemacs-global-set-key (kbd "C-x <f9>") 'find-lisp-find-dired-subdirectories)
 
 ;; (flyspell-correct-word-before-point)
 ;;(ergoemacs-global-set-key "\" 'bookmark-jump)
