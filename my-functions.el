b1caa1ede62022205a63044ff397a0aa7bf760b2
teaforthecat <teaforthecat@gmail.com>
2013-03-24 10:09:48 -0500

    move spawn-shells to my-functions

--- my-functions.el
+++ my-functions.el
@@ -1,3 +1,34 @@
+(defun spawn-shell (name &optional cmds)
+  "Invoke shell in buffer NAME with optional list of COMMANDS"
+  (interactive "MName of shell buffer to create: ")
+
+  (if (get-buffer name)
+      (progn 
+        (setq ss-buffer (get-buffer name)))
+    
+    (progn (setq ss-buffer (get-buffer-create 
+                     (generate-new-buffer-name name)))
+           (shell ss-buffer)))
+
+  (pop-to-buffer (get-buffer-create ss-buffer))
+  (loop for cmd in cmds do         
+        (process-send-string nil cmd )
+        )
+  (comint-send-input)
+)
+
+(spawn-shell "django" (list "virtenv;" 
+                            "cd;"
+                            "cd ~/walkerart;" 
+                            "./manage.py runserver;"))
+
+
+(spawn-shell "collections-server" (list "cd ~/ruby/collections;" 
+                                 "bin/rails s;"))
+(spawn-shell "local" (list "cd"))
+
+
+
 (defun shell-clear ()
   (interactive)
   (let ((comint-buffer-maximum-size 0))
