130c7ca36b6262312a7f42b309abf12474e0bf8c
teaforthecat <teaforthecat@gmail.com>
2013-03-24 08:08:38 -0500

    rm eproject key bindings

--- my-keybindings.el
+++ my-keybindings.el
@@ -12,26 +12,6 @@
 
 
 
-
-
-(defmacro .emacs-curry (function &rest args)
-  `(lambda () (interactive)
-     (,function ,@args)))
-
-(defmacro .emacs-eproject-key (key command)
-  (cons 'progn
-        (loop for (k . p) in (list (cons key 4) (cons (upcase key) 1))
-              collect
-              `(global-set-key
-                (kbd ,(format "C-x p %s" k))
-                (.emacs-curry ,command ,p)))))
-
-(.emacs-eproject-key "k" eproject-kill-project-buffers)
-(.emacs-eproject-key "v" eproject-revisit-project)
-(.emacs-eproject-key "b" eproject-ibuffer)
-(.emacs-eproject-key "o" eproject-open-all-project-files)
-
-
 (defun G (key command)
   (global-set-key key command))
  
