02ecb83e382f312c99978c9c4f266c496ebcf275
teaforthecat <teaforthecat@gmail.com>
2013-03-25 00:46:01 -0500

    rm setup from my-functions

--- my-functions.el
+++ my-functions.el
@@ -4,29 +4,17 @@
 
   (if (get-buffer name)
       (progn 
-        (setq ss-buffer (get-buffer name)))
-    
+        (setq ss-buffer (get-buffer name)))    
     (progn (setq ss-buffer (get-buffer-create 
-                     (generate-new-buffer-name name)))
+                            (generate-new-buffer-name name)))
            (shell ss-buffer)))
 
   (pop-to-buffer (get-buffer-create ss-buffer))
-  (loop for cmd in cmds do         
-        (process-send-string nil cmd )
-        )
-  (comint-send-input)
-)
-
-(spawn-shell "django" (list "virtenv;" 
-                            "cd;"
-                            "cd ~/walkerart;" 
-                            "./manage.py runserver;"))
-
-
-(spawn-shell "collections-server" (list "cd ~/ruby/collections;" 
-                                 "bin/rails s;"))
-(spawn-shell "local" (list "cd"))
-
+  (if cmds
+      (progn
+        (loop for cmd in cmds do         
+              (process-send-string nil cmd ))
+        (comint-send-input))))
 
 
 (defun shell-clear ()
