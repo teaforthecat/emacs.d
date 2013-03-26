c96ab97a676746c404fb8fc8112a4d925eab4e20
Chris Thompson <chris.thompson@walkerart.org>
2013-03-26 18:22:50 -0500

    uh is too common for key-chord

--- init/init-key-chord.el
+++ init/init-key-chord.el
@@ -1,4 +1,4 @@
 (key-chord-mode t)
 (key-chord-define-global "hj"     'undo)
-(key-chord-define-global "uh"     'er/expand-region)
+(key-chord-define-global ".h"     'er/expand-region)
 (key-chord-define-global "jj"     'ace-jump-mode)
