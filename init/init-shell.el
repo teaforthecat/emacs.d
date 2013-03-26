07834c206e0b537f1222707395d4c4e3e21af38c
Chris Thompson <chris.thompson@walkerart.org>
2013-03-26 18:22:26 -0500

    add note to init-shell

--- init/init-shell.el
+++ init/init-shell.el
@@ -6,3 +6,8 @@
 
 (add-hook 'comint-output-filter-functions 'comint-truncate-buffer)
 
+;; needs testing:
+;; (add-hook 'comint-mode-hook
+;;           (lambda ()
+;;             (set (make-local-variable 'comint-file-name-prefix)
+;;                  (or (file-remote-p default-directory) ""))))
