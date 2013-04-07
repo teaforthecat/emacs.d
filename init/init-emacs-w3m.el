b4c0f2479a431a50c1b44936d68d9e4cd7eaff46
teaforthecat <teaforthecat@gmail.com>
2013-04-07 13:49:40 -0500

    require w3m-search fails on mac- path problem again?

--- init/init-emacs-w3m.el
+++ init/init-emacs-w3m.el
@@ -1,3 +1,11 @@
-(setq w3m-use-cookies t)
 (if (string-match "apple-darwin" system-configuration)
     (setq w3m-command "/usr/local/bin/w3m"))
+
+;;(require 'w3m-search)
+
+(setq w3m-use-cookies t)
+
+;;(add-to-list 'w3m-search-engine-alist
+;;             '("emacs-wiki" "http://www.emacswiki.org/cgi-bin/wiki.pl?search=%s"))
+
+ 
