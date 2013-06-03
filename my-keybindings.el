4bb285cb462f4f9c7b0e6bd5bbfcd248f65970fe
cthompson <chris.thompson@govdelivery.com>
2013-06-02 19:02:04 -0500

    keybinings

--- my-keybindings.el
+++ my-keybindings.el
@@ -38,17 +38,18 @@
 (M> h 'backward-char 'beginning-of-line)
 (M> n 'forward-char 'end-of-line)
 (M> s 'ace-jump-mode 'isearch-forward)                    ;new
-(M> - 'comment-dwim)                     ;timid
+(M> - 'comment-dwim)                                      ;timid
+(M  d 'nil )                                              ;unsure
 ;bmwvz
-(M> b 'keyboard-quit)
+(M> b 'ido-switch-buffer)
 (M> m 'previous-buffer)
 (M> w 'next-buffer)
-(M> v 'eproject-ibuffer 'ibuffer)                 ;new
+(M> v 'eproject-ibuffer 'ibuffer)       ;new
 
 ;fgcrl/=\
 (M> r 'subword-forward 'end-of-line)
 (M> g 'subword-backward 'beginning-of-line)
-(M> l 'recenter-top-bottom)              ;timid
+(M> l 'recenter-top-bottom)             ;timid
 (M> c 'previous-line 'scroll-down)
 (M> = 'count-words-region)
 ;67890[]
@@ -71,16 +72,18 @@
 ;yp.,'
 (M ";" 'undo-tree-undo)
 (M ":" 'undo-tree-redo)
-;; (M y   'yank-pop)        ;default 
+;; (M y   'yank-pop)        ;default
 (M p   'subword-kill)
 (M "." 'subword-backward-kill)
 ;; (M "'" 'ido-switch-buffer-other-window) ;timid
 (M "'" 'speedbar)
+(M "'" 'speedbar)
+
 ;54321`
 (M 5 'query-replace)
 (M 4 'split-window-horizontally)
 (M 3 'delete-other-windows)
-(M "`" 'switch-to-next-frame)           ;timid
+(M "`" 'speedbar-get-focus) ;; new
 (M <f5> 'flyspell-correct-word-before-point)
 (M <f6> 'whitespace-cleanup)
 (M <f7> 'pianobar)
@@ -98,22 +101,25 @@
 (C n 'forward-page)
 (C s 'save-buffer)
 (C b 'browse-kill-ring)
-
-(C w 'close-current-buffer)
+(C w 'kill-buffer)
 (C r 'comment-or-uncomment-region)
 
 (C f 'sudo-find-file)
 
-; left hand
+; left hand - finders
+(C 6 'my-ido-find-file-in-tag-files)    ;important
 (C 5 'find-lisp-find-dired)             ;important
 (C 4 'find-grep)                        ;important
 (C 3 'find-file-at-point)               ;important
-(C 2 'find-tag)                         ;important
+;;(C 2 'my-ido-find-tag)                  ;broken
+(C 2 'find-file-in-repository)
 (C 1 'ioccur)                           ;important
+(C 7 'rinari-rgrep)
 
 (C p 'backward-page)
 (C i 'indent-for-tab-command)
 (C o 'eproject-find-file 'find-file)
+(C o 'ido-find-file 'ido-find-file-other-window)
 (C a 'mark-whole-buffer)
 
 (C k 'kill-whole-line)
@@ -140,6 +146,13 @@
 (G (kbd "C-c C-x C-j") 'org-clock-goto)
 (G (kbd "C-c r") 'org-capture)
 
+(G (kbd "A-c") 'mc/edit-lines)
+(G (kbd "A->") 'mc/mark-next-like-this)
+(G (kbd "A-<") 'mc/mark-previous-like-this)
+(G (kbd "C-c A-<") 'mc/mark-all-like-this)
+
+
+
 ;; contrib-functions
 (G (kbd "C-c C-r") 'rename-file-and-buffer)
 
@@ -152,7 +165,7 @@
      (define-key dired-mode-map (kbd "M-g" ) `keyboard-quit)
      (define-key dired-mode-map "e" `dired-up-directory)
      (define-key dired-mode-map "o" `dired-display-file)
-     (define-key dired-mode-map (kbd "M-o") `eproject-find-file)
+     (define-key dired-mode-map (kbd "M-o") `find-file)
      (define-key dired-mode-map "k" `dired-kill-subdir)))
 
 
@@ -165,6 +178,16 @@
      (define-key ioccur-mode-map "N" `ioccur-scroll-down)
      (define-key ioccur-mode-map "P" `ioccur-scroll-up)))
 
+
+
+(eval-after-load 'isearch
+  '(progn
+     (define-key isearch-mode-map (kbd "C-o")
+       (lambda ()
+         (interactive)
+         (let ((case-fold-search isearch-case-fold-search))
+           (ioccur (if isearch-regexp isearch-string
+                    (regexp-quote isearch-string))))))))
 
 
 (eval-after-load 'ibuffer
@@ -203,10 +226,11 @@
 (define-key minibuffer-local-map (kbd "C-.") 'backward-kill-word)
 
 
-
-(if (string-match "apple-darwin" system-configuration)
-    (global-set-key [f11] 'ns-toggle-fullscreen)
-  (global-set-key [f11] 'fullscreen))
+;; apparently fullscreen.el provides for both systems
+;; (if (string-match "apple-darwin" system-configuration)
+;;     (global-set-key [f11] 'ns-toggle-fullscreen)
+;;   (global-set-key [f11] 'fullscreen))
+(global-set-key [f11] 'fullscreen)
 
 (provide 'my-keybindings)
 
