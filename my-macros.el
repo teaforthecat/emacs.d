1b84fce202bce00bf71f2c551ce8ce04d7ff9d3c
cthompson <chris.thompson@govdelivery.com>
2013-06-02 19:02:21 -0500

    rexpects macro

--- my-macros.el
+++ my-macros.el
@@ -21,9 +21,15 @@
    [?\C-  ?\M-g ?\" ?\M-h ?\C-  ?\M-g ?\{ ?\M-h ?# ?\C-u ?\C-  ?\M-n])
 
 
+(fset 'rexpects
+   (lambda (&optional arg) "turn real method call into an expected one of the mocha kind" (interactive "p") (kmacro-exec-ring-item (quote ([101 120 112 101 99 116 115 32 58 134217832 134217832 67108896 134217811 32 134217811 32 13 40 21 67108896 134217838 134217845 134217787 46 114 101 116 117 110 115 32] 0 "%d")) arg)))
+
+
 ;; (fset 'start-server
 ;;    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217825 115 104 101 tab return 118 105 114 116 101 110 118 return 99 100 return 99 100 32 119 tab 97 tab return 46 47 109 97 110 tab 117 110 backspace backspace 114 117 110 115 101 114 118 101 114 return] 0 "%d")) arg)))
 ;; (fset 'start-server
 ;;    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217825 115 104 101 108 108 return 99 100 return 99 100 32 119 97 108 107 101 114 97 114 116 return 118 105 114 116 101 110 118 return 46 47 109 97 110 97 103 101 46 112 121 32 114 117 110 115 101 114 118 101 114 return] 0 "%d")) arg)))
+
+
 
 (provide 'my-macros)
