199ce9c4a810a5a0009fa56d483e00e0c6beb171
Chris Thompson <chris.thompson@govdelivery.com>
2013-11-16 16:42:26 -0600

    add powerline init

---
+++ init/init-powerline.el
@@ -0,0 +1,7 @@
+(setq powerline-color1 "gray30")
+(setq powerline-color2 "gray45")
+(setq powerline-arrow-shape 'curve);;or 'arrow
+(set-face-attribute 'mode-line nil :box nil)
+;; (set-face-attribute 'mode-line nil
+;;     :background "gray22"
+;;     :foreground "F0DFAF" :box nil)
