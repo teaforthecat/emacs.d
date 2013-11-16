713518af94167d416cc5ae37701cd00e695773ec
Chris Thompson <chris.thompson@govdelivery.com>
2013-11-16 16:46:29 -0600

    functions for zeus rinari-console overwrite!?!

--- init/init-rinari.el
+++ init/init-rinari.el
@@ -7,3 +7,24 @@
 (setq rails-ui:show-mode-line nil)
 
 (setq rinari-tags-file-name "TAGS")
+
+(defun rinari-console (&optional edit-cmd-args)
+  "Run a Rails console in a compilation buffer.
+The buffer will support command history and links between errors
+and source code.  Optional prefix argument EDIT-CMD-ARGS lets the
+user edit the console command arguments."
+  (interactive "P")
+  (let* ((default-directory (rinari-root))
+         (command (rinari--wrap-rails-command "console")))
+
+    ;; Start console in correct environment.
+    (when rinari-rails-env
+      (setq command (concat command " " rinari-rails-env)))
+
+    ;; For customization of the console command with prefix arg.
+    (setq command (or edit-cmd-args
+                      command))
+    (with-current-buffer (run-ruby command "zeus console")
+      (dolist (var '(inf-ruby-prompt-pattern inf-ruby-first-prompt-pattern))
+        (set (make-local-variable var) rinari-inf-ruby-prompt-pattern))
+      (rinari-launch))))
--- init/init-ruby-mode.el
+++ init/init-ruby-mode.el
@@ -3,6 +3,38 @@
 (add-to-list 'auto-mode-alist '("gemspec" . ruby-mode))
 (add-to-list 'auto-mode-alist '("json_builder" . ruby-mode))
 
+(defvar zeus-fast-rails "SF=true PAGER=cat DYLD_LIBRARY_PATH=$ORACLE_HOME USE_RAILS_3=true ")
+(defun zeus-start ()
+  (interactive)
+  (spawn-shell "*zeus-start*" '(". ~/.bash_profile" "cd .")) ; rvm loading fails !
+  (insert (concat zeus-fast-rails " zeus start"))
+  (comint-send-input)
+  (buffer-disable-undo))
+
+
+
+;; robe needs to set up a sentinel to set robe-running when buffer killed
+;; (require 'robe)
+;; (defun zeus-console ()
+;;   (interactive)
+;;   (rinari-console "zeus console")
+;;   (robe-start))
+
+
+
+;; @wip
+;; (add-to-list 'align-rules-list '((ruby-hash
+;;                                   (regexp . "=>")
+;;                                   (group 1)
+;;                                   (modes '(ruby-mode)))))
+;;   (python-assignment
+;;   (regexp . "[^=!<> 	\n]\\(\\s-*\\)=\\(\\s-*\\)\\([^>= 	\n]\\|$\\)")
+;;   (group 1 2)
+;;   (modes quote
+;;          (python-mode))
+;;   (tab-stop))
+;; )
+
 ;; (add-hook 'ruby-mode-hook
 ;;           (lambda ()
 ;;             (rvm-activate-corresponding-ruby)
