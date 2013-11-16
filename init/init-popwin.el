9388fae60b9475da1b561305c1c0644a91c8b181
Chris Thompson <chris.thompson@govdelivery.com>
2013-11-16 16:30:11 -0600

    add popwin

---
+++ init/init-popwin.el
@@ -0,0 +1,9 @@
+(popwin-mode 1)
+
+;; M-!
+(push "*Shell Command Output*" popwin:special-display-config)
+
+
+
+;; undo-tree
+(push '(" *undo-tree*" :width 0.3 :position right) popwin:special-display-config)
