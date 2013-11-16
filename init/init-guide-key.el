cf41fb16b32e91c30f52167f5f78a873f0912bac
Chris Thompson <chris.thompson@govdelivery.com>
2013-11-16 16:30:47 -0600

    add guide-key init

---
+++ init/init-guide-key.el
@@ -0,0 +1,7 @@
+(require 'guide-key)
+(setq guide-key/idle-delay 0.2)
+(guide-key-mode -1)
+(setq guide-key/guide-key-sequence '("C-x r" "C-c &"))
+;(setq guide-key/recursive-key-sequence-flag nil)
+(setq guide-key/popup-window-position 'bottom)
+
