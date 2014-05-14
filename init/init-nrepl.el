97098262b60be5bd15c02289355845b8783fdd57
Chris Thompson <chris.thompson@govdelivery.com>
2014-05-13 22:10:01 -0500

    enabling terminal

--- init/init-nrepl.el
+++ init/init-nrepl.el
@@ -1,20 +1,22 @@
-;(autopair-mode -1)
-;(paredit-mode -1)
-;(smartparens-mode)
-;(smartparens-strict-mode)
-
-(define-key nrepl-mode-map (kbd "C-.") 'nrepl-previous-input)
-
-(define-key nrepl-mode-map (kbd "M-.") 'backward-kill-word)
+;;(autopair-mode -1)
+;;(paredit-mode -1)
+;;(smartparens-mode)
+;;(smartparens-strict-mode)
 ;;(undefine-key nrepl-mode-map (kbd "M-."))
-(define-key nrepl-mode-map (kbd "C-j") 'nrepl-jump)
+;; (eval-after-load 'nrepl (progn
+;;                           (define-key nrepl-mode-map (kbd "C-.") 'nrepl-previous-input)
+
+;;                           (define-key nrepl-mode-map (kbd "M-.") 'backward-kill-word)
+
+;;                           (define-key nrepl-mode-map (kbd "C-j") 'nrepl-jump)
 
 
-(add-hook 'nrepl-interaction-mode-hook
-  'nrepl-turn-on-eldoc-mode)
+;;                           (add-hook 'nrepl-interaction-mode-hook
+;;                                     'nrepl-turn-on-eldoc-mode)
 
-(setq nrepl-popup-stacktraces nil)
+;;                           (setq nrepl-popup-stacktraces nil)
 
-(add-to-list 'same-window-buffer-names "*nrepl*")
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+;;                           (add-to-list 'same-window-buffer-names "*nrepl*")
+;;                           (add-hook 'nrepl-mode-hook 'paredit-mode)
+;;                           (add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+;;                           ))
