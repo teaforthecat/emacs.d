7528cc031a44846e3d1368e36d0158395d7ef59e
Chris Thompson <chris.thompson@govdelivery.com>
2014-05-13 21:58:07 -0500

    enabling terminal

--- init/init-guide-key.el
+++ init/init-guide-key.el
@@ -1,10 +1,13 @@
 (require 'guide-key)
-(require 'diminish)
-;(setq guide-key/idle-delay 0.2)
-(guide-key-mode 1)
-(diminish 'guide-key-mode)
-;(setq guide-key/guide-key-sequence '("\C-x r" "\C-c &" "\C-xw"))
+
+(add-hook 'el-get-post-init-hooks '(lambda ()
+                                    (require 'diminish)
+                                    (guide-key-mode 1)
+                                    (diminish 'guide-key-mode)))
+
 (setq guide-key/guide-key-sequence '("\C-x 8" ;;internationalization
                                      "H-p"))  ;; smartparens
+;(setq guide-key/idle-delay 0.2)
+;(setq guide-key/guide-key-sequence '("\C-x r" "\C-c &" "\C-xw"))
 ;(setq guide-key/recursive-key-sequence-flag nil)
 ;(setq guide-key/popup-window-position 'bottom)
