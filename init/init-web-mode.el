908cb028f8b50dc1ee4a133bfbec974547c655de
Chris Thompson <chris.thompson@govdelivery.com>
2014-09-21 11:12:29 -0500

    unset webmode keymap

--- init/init-web-mode.el
+++ init/init-web-mode.el
@@ -1,2 +1,4 @@
 (add-to-list 'auto-mode-alist '("\\.hbs$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+(setq web-mode-map (make-sparse-keymap))
+(define-key web-mode-map (kbd "M-;") 'undo-tree-undo)
